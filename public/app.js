a53989db18ca94fa8cac95e3bc3b183d682b202a
eesypeesy <46086523+eesypeesy@users.noreply.github.com>
2020-06-13 22:24:09 +0530

    better offline animations🎀

--- public/app.js
+++ public/app.js
@@ -3,7 +3,19 @@ async function run(){
     const date = new Date();
     const time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
     const month = date.getMonth() + 1;
-    const day = date.getDate(); 
+    const day = date.getDate();
+    document.getElementById("fetching2").innerHTML= "(this usually takes less than 5 seconds)";
+    setTimeout(() => {
+        if(!window.navigator.onLine){document.getElementById("fetching2").innerHTML= "(deploying bigger and faster antennas)";
+        document.getElementById("head").innerHTML= "still trying";
+        document.getElementById("load").style.animation = "rotate .5s ease-in-out alternate-reverse infinite";
+    }
+    },10000);
+    setTimeout(() => {
+        if(!window.navigator.onLine){
+        document.getElementById("fetching").innerHTML= "fetching failed :( <div id='fetching2'>please check your connection and reload </div>";
+        document.getElementById("load").style.display = "none";}
+    },20000);
 
     let response = await fetch('https://disease.sh/v2/all');
     response.json().then( (data) => {
